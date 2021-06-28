457b8c020f288328a4c8cf607281fe74703a3701
sanar311 <43620675+sanar311@users.noreply.github.com>
2021-06-27 23:41:36 -0500

    Added the AWS Certication Title and challenge date

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to AWS_Certified_Developer_Associate-DVA-C01'));
+app.get('/', (req, res) => res.send('AWS_Certified_Developer_Associate-DVA-C01-Challenge Dec04,2021'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
